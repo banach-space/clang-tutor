fca3809ec631d952edcae7866f51830603181ff2
Chenhao <tylzh1997@vip.qq.com>
2025-07-14 02:06:00 +0000

    Fixed an issue where Clang Tools were used in actual compilation processes; modified the invocation method of the CodeStyleChecker plugin for lit test cases and adjusted the corresponding lit tests.

--- test/CodeStyleCheckerConversionOp.cpp
+++ test/CodeStyleCheckerConversionOp.cpp
@@ -1,13 +1,22 @@
 // RUN: clang -cc1 -verify -load %shlibdir/libCodeStyleChecker%shlibext -add-plugin CSC %s 2>&1
-// RUN: clang -Xclang -load -Xclang %shlibdir/libCodeStyleChecker%shlibext -Xclang -add-plugin -Xclang CSC -c %s 2>&1
-// RUN: clang -fplugin=%shlibdir/libCodeStyleChecker%shlibext -c %s 2>&1
 
-// Verify that conversion operators are not checked
+// RUN: clang -Xclang -load -Xclang %shlibdir/libCodeStyleChecker%shlibext -Xclang -add-plugin -Xclang CSC -c %s -o %t.o
+// RUN: test -f %t.o
+// RUN: rm %t.o
 
-// expected-no-diagnostics
-class SomeClass {
-public:
-  operator bool();
-  operator int();
-  operator char();
+// RUN: clang -fplugin=%shlibdir/libCodeStyleChecker%shlibext -c %s -o %t.o
+// RUN: test -f %t.o
+// RUN: rm %t.o
+
+// Verify that function names starting with upper case are reported as invalid
+
+// expected-warning@+1 {{Function names should start with lower-case letter}}
+void ClangTutorFuncBad();
+
+void clangTutorFuncOK();
+
+struct ClangTutorStruct {
+  // expected-warning@+1 {{Function names should start with lower-case letter}}
+  void ClangTutorMemberMethodBad();
+  void clangTutorMemberMethodOK();
 };
